f533131ce74da0ff8fe049518f2ba4a593a479f1
pavithra <pavithrasanthoshi07@gamil.com>
2026-04-07 09:59:33 +0530

    UC10 : Count Total Seats in Train

--- App/src/TrainManagementApp.java
+++ App/src/TrainManagementApp.java
@@ -1,5 +1,5 @@
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 // Bogie class (reuse)
 class Bogie {
@@ -10,14 +10,9 @@ class Bogie {
         this.name = name;
         this.capacity = capacity;
     }
-
-    @Override
-    public String toString() {
-        return name + " -> " + capacity;
-    }
 }
 
-public class TrainAppUC9 {
+public class TrainAppUC10 {
 
     public static void main(String[] args) {
 
@@ -28,19 +23,14 @@ public class TrainAppUC9 {
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 60));
         bogies.add(new Bogie("First Class", 24));
-        bogies.add(new Bogie("Sleeper", 72)); // duplicate type
-        bogies.add(new Bogie("AC Chair", 60));
 
-        // Group bogies by type (name)
-        Map<String, List<Bogie>> groupedBogies = bogies.stream()
-                .collect(Collectors.groupingBy(b -> b.name));
+        // Calculate total seating capacity using stream
+        int totalSeats = bogies.stream()
+                .map(b -> b.capacity)        // extract capacity
+                .reduce(0, Integer::sum);   // sum all values
 
-        // Display grouped result
-        System.out.println("\nGrouped Bogies:");
-
-        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
+        // Display result
+        System.out.println("\nTotal Seating Capacity: " + totalSeats);
 
         System.out.println("\nProgram continues...");
     }
